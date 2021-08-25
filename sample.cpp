992f27078fe14b85b3cc5a2696132ea8bb2dca53
Matiyas Seifu <88274941+mati1yas@users.noreply.github.com>
2021-08-25 14:27:34 +0300

    Update sample.cpp
    these structures are to hold date of issue and return for the issued books  ,address of the user and to data of password  and username for library adminstrators

--- sample.cpp
+++ sample.cpp
@@ -1,5 +1,18 @@
 #include<iostream>
 using namespace std;
+
+struct Date{
+int yy,mm,dd;
+};
+struct Address{
+int house_no;
+int phone;
+};
+
+struct Admin{
+string password,user_name;
+};
+
 int main(){
 
 
