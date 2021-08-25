4bc3e5527e33e0520ae6fe6dd1031f2420fefb0f
mehari123 <88460961+mehari123@users.noreply.github.com>
2021-08-25 16:52:57 +0300

    book & user struct data type
    I have added a struct data type which is used to record details about the book found in library( or when the library add new), I have defined it at the time of declaration because we use this information every where so it should be global!, the array size can be changed. the second struct is to input user (reader) information.

--- sample.cpp
+++ sample.cpp
@@ -13,7 +13,26 @@ struct Admin{
 string password,user_name;
 };
 
+// to recored a book details that found in library
+struct Book{
+string book_id,title,author,shelf_no,catagory;
+int quantity,isbn,edition;
+Date date_of_pub;
+float rate;// the rate given by readers, based on thier satisfaction on the book.
+int read_freq;
+bool issued;
+}book[1000];
+
+//to recored any library users information / or to recored library member informaion.
+struct User{
+string name, user_id;
+int age;
+Address user_address;
+int num_issued_book;
+Book issued_book[2];// the user can only borrow 2 books at the same time( this is why it is array).
+};
 int main(){
+  
 
 
 return 0;
