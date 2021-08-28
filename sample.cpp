6e76755a8c6e4e77e4d88ccdeb0bdd90e5316632
Mahlet Tizazu <87749526+mahi16tz@users.noreply.github.com>
2021-08-28 11:32:16 +0300

    Update sample.cpp
    I have edited the main menu function for program simplicity and display clarity and also I  have added another function that adds users who want to be member of the library.

--- sample.cpp
+++ sample.cpp
@@ -1,4 +1,8 @@
 #include<iostream>
+#include<iomanip>
+#include<stdlib.h>
+#include<string.h>
+#include<string>
 using namespace std;
 
 struct Date{
@@ -35,14 +39,13 @@ Book issued_book[2];// the user can only borrow 2 books at the same time( this i
 //Declaration of Functions  
 void welcome();
 char user_option();
-int main_menu();
+void main_menu();
+void memberRegistration();
 
 
 int main(){
  welcome(); 
-
- int choose;
-choose=main_menu();
+main_menu();
 do{
 
 switch(choose){
@@ -92,16 +95,31 @@ cout<<"\n\n\n";
 
 //This is a function that enables the person who access is whether a user or admin
 int main_menu(){
-int choose; 
-cout<<"\n\n\n\n\t\t\t***********************************************************************\n";
-cout<<"\t\t\t\t 5M library management system\t\t\t\t\t\t\t\t                                 \n";
-cout<<"\t\t\t*************************************************************************\n";
+system ("cls");
+ int choice;
+       cout <<"\n\n\n\n"<<endl;
+        cout << setw(15)<< char(176);      for(int i=0; i<90; i++){cout << char(205);} cout << char(176)<< endl;
+        cout << setw(10)<< char(176)<<"select User or Admin to from the following  "<<endl;
+        cout << setw(15)<< char(176)<<"1. User"<<endl;
+        cout << setw(15)<< char(176)<<"2. Admin"<<endl;
+        cout << setw(15)<< char(176)<<"3. Exit"<<endl;
+        cout << setw(15)<< char(176);      for(int i=0; i<90; i++){cout << char(205);} cout << char(176)<< endl;
+        cin>>choice;
 
-cout<<"\t\t\t1: Admin\n\t\t\t2: User\n\t\t\t3: Exit \n\n\n";
-cout<<"\t\t\t>> please insert your choice ---> ";
-cin>>choose;
 
-return choose;
+         switch (choice){
+    case 1:
+      user_option();
+        break;
+
+    case 2:
+    admin_option ();
+        break;
+    }
+
+
+}
+
 
 
 }
@@ -153,5 +171,29 @@ cout<<"you enterd inappropriate character.please try again!!"}
 }
         return choice;
 }
+ // This is a function to register multiple members at once
+void memberRegistration(){
 
+        addanother:
+
+
+         cout<<"Enter Name : "; getline(cin,user[Ucounter].name);
+         cout<<"Enter ID : " ; getline(cin,user[Ucounter].user_id);
+         cout<<"Enter Age : ";  cin>>user[Ucounter].age;
+         cout<<"Enter House no. : "; cin>>user[Ucounter]. user_address.house_no;
+         cout<<"Enter Phone no. : ";  cin>>user[Ucounter]. user_address.phone;
+
+         cout<<"Member added successfully !"<<endl;
+         Ucounter++;
+         int choice ;
+
+    cout<<"Do you want to Add another member 1(yes ) or 2(NO) "; cin>>choice;
+    cin.ignore();
+       system ("cls");
+
+       if (choice==1)
+        goto addanother;
+
+
+}
 
